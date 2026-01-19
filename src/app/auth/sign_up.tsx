532ab48786bbf176536337ad1ff5748e7a155c7d
istoria <88208629+istoria-eureka@users.noreply.github.com>
2026-01-20 04:40:04 +0900

    Create create.tsx log_in.tsx sing_up.tsx

---
+++ src/app/auth/sign_up.tsx
@@ -0,0 +1,80 @@
+import { View, Text, TextInput, StyleSheet } from "react-native";
+import { JSX } from "react";
+
+import Header from "../../components/Header";
+
+const SignUp = (): JSX.Element => {
+    return (
+        <View style={styles.container} >
+            <Header />
+            <View style={styles.inner}>
+                <Text style={styles.title} >Sign Up</Text>
+                <TextInput value="Email address" style={styles.input} />
+                <TextInput value="Password" style={styles.input} />
+                <View style={styles.button} >
+                    <Text style={styles.buttonLabel} >Submit</Text>
+                </View>
+                <View style={styles.footer}>
+                    <Text style={styles.footerText} >Already registerd?</Text>
+                    <Text style={styles.footerLink} >Log In.</Text>
+                </View>
+            </View>
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#F0F4F8"
+    },
+    inner: {
+        paddingVertical: 24,
+        paddingHorizontal: 27
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: "bold",
+        lineHeight: 32,
+        marginBottom: 24
+    },
+    input: {
+        backgroundColor: "#FFF",
+        borderWidth: 1,
+        borderColor: "#DDD",
+        color: "#DDD",
+        fontSize: 16,
+        height: 48,
+        padding: 8,
+        marginBottom: 16
+    },
+    button: {
+        alignSelf: "flex-start",
+        backgroundColor: "#467FD3",
+        borderRadius: 4,
+        marginBottom: 24
+    },
+    buttonLabel: {
+        color: "#FFF",
+        fontSize: 16,
+        lineHeight: 32,
+        paddingVertical: 8,
+        paddingHorizontal: 24,
+    },
+    footer: {
+        flexDirection: "row"
+    },
+    footerText: {
+        fontSize: 14,
+        lineHeight: 24,
+        marginRight: 8,
+        color:"#000"
+    },
+    footerLink: {
+        color: "#467FD3",
+        fontSize: 14,
+        lineHeight: 24
+    }
+});
+
+export default SignUp;
